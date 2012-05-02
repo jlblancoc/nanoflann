01d53671d5183173e8d07fce58c26a1930d25290
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2012-05-02 11:01:47 +0000

    fix a build error in the performance test

--- perf-tests/nanoflann/test_nanoflann.cpp
+++ perf-tests/nanoflann/test_nanoflann.cpp
@@ -145,7 +145,7 @@ void perf_test(const size_t N)
 	t0=get_time();
 
 	const size_t num_results = 1;
-	int ret_index;
+	size_t ret_index;
 	num_t out_dist_sqr;
 	nanoflann::KNNResultSet<num_t> resultSet(num_results);
 	resultSet.init(&ret_index, &out_dist_sqr );
