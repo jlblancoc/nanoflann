902950cadde74133fd7c4bcb9f5eaa43f579d423
Jose Luis Blanco <joseluisblancoc@gmail.com>
2014-05-01 17:58:50 +0200

    added travis support

---
+++ .travis.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SRC_DIR=`pwd`
+BUILD_DIR=build
+
+CMAKE_C_FLAGS="-Wall -Wextra -Wabi -O2"
+CMAKE_CXX_FLAGS="-Wall -Wextra -Wabi -O2"
+
+function build ()
+{
+  mkdir $BUILD_DIR && cd $BUILD_DIR
+  cmake $SRC_DIR
+  make -j2
+}
+
+case $TASK in
+  build ) build;;
+esac
