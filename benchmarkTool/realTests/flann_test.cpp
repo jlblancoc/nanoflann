b34088f4e25d0fa852a41b8b35d6a1e9f6e82df9
pranjalr34 <pranjalr34@gmail.com>
2017-07-06 01:32:05 +0530

    Added realTests file for flann

--- benchmarkTool/realTests/flann_test.cpp
+++ benchmarkTool/realTests/flann_test.cpp
@@ -26,58 +26,18 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *************************************************************************/
 
-#include <nanoflann.hpp>
+#include <flann/flann.hpp>
 #include <ctime>
 #include <cstdlib>
 #include <iostream>
-#include<bits/stdc++.h>
+#include <fstream>
+
 using namespace std;
-using namespace nanoflann;
-
-// This is an exampleof a custom data set class
-template <typename T>
-struct PointCloud
-{
-	struct Point
-	{
-		T  x,y,z;
-	};
-
-	std::vector<Point>  pts;
-
-	// Must return the number of data points
-	inline size_t kdtree_get_point_count() const { return pts.size(); }
-
-	// Returns the distance between the vector "p1[0:size-1]" and the data point with index "idx_p2" stored in the class:
-	inline T kdtree_distance(const T *p1, const size_t idx_p2,size_t /*size*/) const
-	{
-		const T d0=p1[0]-pts[idx_p2].x;
-		const T d1=p1[1]-pts[idx_p2].y;
-		const T d2=p1[2]-pts[idx_p2].z;
-		return d0*d0+d1*d1+d2*d2;
-	}
-
-	// Returns the dim'th component of the idx'th point in the class:
-	// Since this is inlined and the "dim" argument is typically an immediate value, the
-	//  "if/else's" are actually solved at compile time.
-	inline T kdtree_get_pt(const size_t idx, int dim) const
-	{
-		if (dim==0) return pts[idx].x;
-		else if (dim==1) return pts[idx].y;
-		else return pts[idx].z;
-	}
-
-	// Optional bounding-box computation: return false to default to a standard bbox computation loop.
-	//   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it again.
-	//   Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
-	template <class BBOX>
-	bool kdtree_get_bbox(BBOX& /* bb */) const { return false; }
-
-};
+using namespace flann;
 
 // Scan all points from file
 template <typename T>
-void scanPointCloud(PointCloud<T> &point, string file)
+void scanPointCloud(Matrix<T> &point, unsigned int &N, string file)
 {
         ifstream read(file.c_str());
         
@@ -87,93 +47,78 @@ void scanPointCloud(PointCloud<T> &point, string file)
         vector<vector<T> > cloud;
         vector<T> tmp;
         
-        double x,y,z,d;
-        unsigned int N=0;
+        T x,y,z,d;
+        N=0;
         while(read>>x>>y>>z>>d){
             tmp.clear();
             tmp.push_back(x); tmp.push_back(y); tmp.push_back(z);
             cloud.push_back(tmp);
             N++;
         }
-	point.pts.resize(N);
-	for (size_t i=0;i<N;i++)
-	{
-		point.pts[i].x = cloud[i][0];
-		point.pts[i].y = cloud[i][1];
-		point.pts[i].z = cloud[i][2];
+        random_shuffle(cloud.begin(), cloud.end());
+    	Matrix<T> Point(new T[N*3], N, 3);
+    	for (size_t i=0;i<N;i++)
+    	{
+    		Point[i][0] = cloud[i][0];
+    		Point[i][1] = cloud[i][1];
+    		Point[i][2] = cloud[i][2];
         }
+        point=Point;
 }
 
 
 template <typename num_t>
 void kdtree_demo(string &path)
 {
-	PointCloud<num_t> PcloudS, PcloudT;
-	// Scan points from file:
-	scanPointCloud<num_t>(PcloudS, path+"scan1.dat");
-        // Randomly shuffle the data
-        random_shuffle(PcloudS.pts.begin(), PcloudS.pts.end());
-	
-        scanPointCloud<num_t>(PcloudT, path+"scan2.dat");
+        int nn=1;
+        Matrix<num_t> PcloudS, PcloudT;
+        unsigned int N;
+        // Scan points from file:
+        scanPointCloud<num_t>(PcloudS, N, path+"scan1.dat");
+        scanPointCloud<num_t>(PcloudT, N, path+"scan2.dat");
 
-        num_t query_pt[3];
-
-        const unsigned int N = PcloudS.pts.size();
-	
+        Matrix<num_t> query(new num_t[1*3], 1, 3);
+    
         // buildTime : time required to build the kd-tree index
         // queryTime : time required to find nearest neighbor for a single point in the kd-tree
         vector<double> buildTime, queryTime;
 
         int plotCount=10;
-
         for(int i=1;i<=plotCount;i++)
         {
-	    PointCloud<num_t> cloudS, cloudT;
-
             // size of dataset currently being used
             int currSize=((i*1.0)/plotCount)*N;
+            Matrix<num_t> cloudS(new num_t[currSize*3], currSize, 3);
+            Matrix<num_t> cloudT(new num_t[currSize*3], currSize, 3);
 
-            cloudS.pts.resize(currSize);
-            cloudT.pts.resize(currSize);
             for(int j=0;j<currSize;j++)
             {
-                cloudS.pts[j]=PcloudS.pts[j];
-                cloudT.pts[j]=PcloudT.pts[j];
+                for(int k=0;k<3;k++)
+                {
+                    cloudS[j][k]=PcloudS[j][k];
+                    cloudT[j][k]=PcloudT[j][k];
+                }
             }
 
-            //	 construct a kd-tree index:
-            typedef KDTreeSingleIndexAdaptor<
-                L2_Simple_Adaptor<num_t, PointCloud<num_t> > ,
-                PointCloud<num_t>,
-                3 /* dim */
-                    > my_kd_tree_t;
-
-            my_kd_tree_t   index(3 /*dim*/, cloudS, KDTreeSingleIndexAdaptorParams(10 /* max leaf */) );
-            
+            Index<L2<num_t> > index(cloudS, flann::KDTreeIndexParams(1));
             clock_t begin = clock();
-            
             index.buildIndex();
-            
             clock_t end = clock();
             double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
             buildTime.push_back(elapsed_secs);
-
             {
+
                 clock_t begin = clock();
 
                 for(int j=0;j<currSize;j++)
                 {
-                    query_pt[0]=cloudT.pts[j].x;
-                    query_pt[1]=cloudT.pts[j].y;
-                    query_pt[2]=cloudT.pts[j].z;
-                    
-                    // do a knn search
-                    const size_t num_results = 1;
-                    size_t ret_index;
-                    num_t out_dist_sqr;
-                    nanoflann::KNNResultSet<num_t> resultSet(num_results);
-                    resultSet.init(&ret_index, &out_dist_sqr );
-                    index.findNeighbors(resultSet, &query_pt[0], nanoflann::SearchParams(10));
+                    query[0][0]=cloudT[j][0];
+                    query[0][1]=cloudT[j][1];
+                    query[0][2]=cloudT[j][2];
+                    Matrix<int> indices(new int[query.rows*nn], query.rows, nn);
+                    Matrix<num_t> dists(new num_t[query.rows*nn], query.rows, nn);
+
+                    index.knnSearch(query, indices, dists, nn, flann::SearchParams(-1));
                 }
 
                 clock_t end = clock();
