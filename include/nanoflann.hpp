1e973d5595f5d70b55d7870170a6b0be5522c64d
Geoffrey Lalonde <glalonde@google.com>
2018-02-14 20:17:13 -0800

    dynamic tree constness and support initial points in cloud

--- include/nanoflann.hpp
+++ include/nanoflann.hpp
@@ -1471,7 +1471,7 @@ namespace nanoflann
 		/**
 		 * The dataset used by this index
 		 */
-		DatasetAdaptor &dataset; //!< The source of our data
+		const DatasetAdaptor &dataset; //!< The source of our data
 
 		KDTreeSingleIndexAdaptorParams index_params;
 
@@ -1507,7 +1507,7 @@ namespace nanoflann
 		 * @param inputData Dataset with the input features
 		 * @param params Basically, the maximum leaf node size
 		 */
-		KDTreeSingleIndexDynamicAdaptor_(const int dimensionality, DatasetAdaptor& inputData, std::vector<int>& treeIndex_, const KDTreeSingleIndexAdaptorParams& params = KDTreeSingleIndexAdaptorParams()) :
+		KDTreeSingleIndexDynamicAdaptor_(const int dimensionality, const DatasetAdaptor& inputData, std::vector<int>& treeIndex_, const KDTreeSingleIndexAdaptorParams& params = KDTreeSingleIndexAdaptorParams()) :
 			dataset(inputData), index_params(params), treeIndex(treeIndex_), distance(inputData)
 		{
 			BaseClassRef::root_node = NULL;
@@ -1762,7 +1762,7 @@ namespace nanoflann
 		/**
 		 * The dataset used by this index
 		 */
-		DatasetAdaptor &dataset; //!< The source of our data
+		const DatasetAdaptor &dataset; //!< The source of our data
 
 		std::vector<int> treeIndex; //!< treeIndex[idx] is the index of tree in which point at idx is stored. treeIndex[idx]=-1 means that point has been removed.
 
@@ -1818,10 +1818,9 @@ namespace nanoflann
 		 * @param inputData Dataset with the input features
 		 * @param params Basically, the maximum leaf node size
 		 */
-		KDTreeSingleIndexDynamicAdaptor(const int dimensionality, DatasetAdaptor& inputData, const KDTreeSingleIndexAdaptorParams& params = KDTreeSingleIndexAdaptorParams() , const size_t maximumPointCount = 1000000000U) :
+		KDTreeSingleIndexDynamicAdaptor(const int dimensionality, const DatasetAdaptor& inputData, const KDTreeSingleIndexAdaptorParams& params = KDTreeSingleIndexAdaptorParams() , const size_t maximumPointCount = 1000000000U) :
 			dataset(inputData), index_params(params), distance(inputData)
 		{
-			if (dataset.kdtree_get_point_count()) throw std::runtime_error("[nanoflann] cannot handle non empty point cloud.");
 			treeCount = std::log2(maximumPointCount);
 			pointCount = 0U;
 			dim = dimensionality;
@@ -1829,6 +1828,10 @@ namespace nanoflann
 			if (DIM > 0) dim = DIM;
 			m_leaf_max_size = params.leaf_max_size;
 			init();
+			int num_initial_points = dataset.kdtree_get_point_count();
+			if (num_initial_points > 0) {
+				addPoints(0, num_initial_points - 1);
+			}
 		}
 
 
