7797b2c2463ede7ffe0b3be677e7cc998bc4018a
lionel untereiner <lionel.untereiner@inria.fr>
2018-03-15 15:33:01 +0100

    Change specific CArray type to std::array (c++11)

--- include/nanoflann.hpp
+++ include/nanoflann.hpp
@@ -47,6 +47,7 @@
 #define  NANOFLANN_HPP_
 
 #include <vector>
+#include <array>
 #include <cassert>
 #include <algorithm>
 #include <stdexcept>
@@ -69,6 +70,38 @@ namespace nanoflann
 {
 /** @addtogroup nanoflann_grp nanoflann C++ library for ANN
   *  @{ */
+
+	/**
+	 * Traits if object is resizable (typically has a resize method)
+	 */
+  	template <typename T, typename = int>
+	struct is_resizable : std::false_type {};
+
+	template <typename T>
+	struct is_resizable <T, decltype((void) &T::resize, 0)> : std::true_type {};
+
+
+	/**
+	 * Free function to resize a resizable object
+	 */
+	template <typename Container>
+	inline auto resize(Container& c, const size_t nElements) 
+		-> typename std::enable_if<is_resizable<Container>::value>::type
+	{ 
+		c.resize(nElements);		
+	}
+
+	/**
+	 * Free function that has no effects on non resizable containers (e.g. std::array)
+	 * It raises an exception if the expected size does not match
+	 */
+	template <typename Container>
+	inline auto resize(Container& c, const size_t nElements) 
+		-> typename std::enable_if<!is_resizable<Container>::value>::type
+	{ 
+		if(nElements != c.size())
+			throw std::logic_error("Try to change the size of a std::array.");
+	}
 
   	/** Library version: 0xMmP (M=Major,m=minor,P=patch) */
 	#define NANOFLANN_VERSION 0x123
@@ -668,118 +701,13 @@ namespace nanoflann
 	/** @addtogroup nanoflann_metaprog_grp Auxiliary metaprogramming stuff
 	  * @{ */
 
-	// ----------------  CArray -------------------------
-	/** A STL container (as wrapper) for arrays of constant size defined at compile time (class imported from the MRPT project)
-	 * This code is an adapted version from Boost, modifed for its integration
-	 *	within MRPT (JLBC, Dec/2009) (Renamed array -> CArray to avoid possible potential conflicts).
-	 * See
-	 *      http://www.josuttis.com/cppcode
-	 * for details and the latest version.
-	 * See
-	 *      http://www.boost.org/libs/array for Documentation.
-	 * for documentation.
-	 *
-	 * (C) Copyright Nicolai M. Josuttis 2001.
-	 * Permission to copy, use, modify, sell and distribute this software
-	 * is granted provided this copyright notice appears in all copies.
-	 * This software is provided "as is" without express or implied
-	 * warranty, and with no claim as to its suitability for any purpose.
-	 *
-	 * 29 Jan 2004 - minor fixes (Nico Josuttis)
-	 * 04 Dec 2003 - update to synch with library TR1 (Alisdair Meredith)
-	 * 23 Aug 2002 - fix for Non-MSVC compilers combined with MSVC libraries.
-	 * 05 Aug 2001 - minor update (Nico Josuttis)
-	 * 20 Jan 2001 - STLport fix (Beman Dawes)
-	 * 29 Sep 2000 - Initial Revision (Nico Josuttis)
-	 *
-	 * Jan 30, 2004
-	 */
-    template <typename T, std::size_t N>
-    class CArray {
-      public:
-        T elems[N];    // fixed-size array of elements of type T
-
-      public:
-        // type definitions
-        typedef T              value_type;
-        typedef T*             iterator;
-        typedef const T*       const_iterator;
-        typedef T&             reference;
-        typedef const T&       const_reference;
-        typedef std::size_t    size_type;
-        typedef std::ptrdiff_t difference_type;
-
-        // iterator support
-        inline iterator begin() { return elems; }
-        inline const_iterator begin() const { return elems; }
-        inline iterator end() { return elems+N; }
-        inline const_iterator end() const { return elems+N; }
-
-        // reverse iterator support
-#if !defined(BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION) && !defined(BOOST_MSVC_STD_ITERATOR) && !defined(BOOST_NO_STD_ITERATOR_TRAITS)
-        typedef std::reverse_iterator<iterator> reverse_iterator;
-        typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-#elif defined(_MSC_VER) && (_MSC_VER == 1300) && defined(BOOST_DINKUMWARE_STDLIB) && (BOOST_DINKUMWARE_STDLIB == 310)
-        // workaround for broken reverse_iterator in VC7
-        typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, iterator,
-                                      reference, iterator, reference> > reverse_iterator;
-        typedef std::reverse_iterator<std::_Ptrit<value_type, difference_type, const_iterator,
-                                      const_reference, iterator, reference> > const_reverse_iterator;
-#else
-        // workaround for broken reverse_iterator implementations
-        typedef std::reverse_iterator<iterator,T> reverse_iterator;
-        typedef std::reverse_iterator<const_iterator,T> const_reverse_iterator;
-#endif
-
-        reverse_iterator rbegin() { return reverse_iterator(end()); }
-        const_reverse_iterator rbegin() const { return const_reverse_iterator(end()); }
-        reverse_iterator rend() { return reverse_iterator(begin()); }
-        const_reverse_iterator rend() const { return const_reverse_iterator(begin()); }
-        // operator[]
-        inline reference operator[](size_type i) { return elems[i]; }
-        inline const_reference operator[](size_type i) const { return elems[i]; }
-        // at() with range check
-        reference at(size_type i) { rangecheck(i); return elems[i]; }
-        const_reference at(size_type i) const { rangecheck(i); return elems[i]; }
-        // front() and back()
-        reference front() { return elems[0]; }
-        const_reference front() const { return elems[0]; }
-        reference back() { return elems[N-1]; }
-        const_reference back() const { return elems[N-1]; }
-        // size is constant
-        static inline size_type size() { return N; }
-        static bool empty() { return false; }
-        static size_type max_size() { return N; }
-        enum { static_size = N };
-		/** This method has no effects in this class, but raises an exception if the expected size does not match */
-		inline void resize(const size_t nElements) { if (nElements!=N) throw std::logic_error("Try to change the size of a CArray."); }
-        // swap (note: linear complexity in N, constant for given instantiation)
-        void swap (CArray<T,N>& y) { std::swap_ranges(begin(),end(),y.begin()); }
-        // direct access to data (read-only)
-        const T* data() const { return elems; }
-        // use array as C array (direct read/write access to data)
-        T* data() { return elems; }
-        // assignment with type conversion
-        template <typename T2> CArray<T,N>& operator= (const CArray<T2,N>& rhs) {
-            std::copy(rhs.begin(),rhs.end(), begin());
-            return *this;
-        }
-        // assign one value to all elements
-        inline void assign (const T& value) { for (size_t i=0;i<N;i++) elems[i]=value; }
-        // assign (compatible with std::vector's one) (by JLBC for MRPT)
-        void assign (const size_t n, const T& value) { assert(N==n); for (size_t i=0;i<N;i++) elems[i]=value; }
-      private:
-        // check range (may be private because it is static)
-        static void rangecheck (size_type i) { if (i >= size()) { throw std::out_of_range("CArray<>: index out of range"); } }
-    }; // end of CArray
-
 	/** Used to declare fixed-size arrays when DIM>0, dynamically-allocated vectors when DIM=-1.
 	  * Fixed size version for a generic DIM:
 	  */
 	template <int DIM, typename T>
 	struct array_or_vector_selector
 	{
-		typedef CArray<T, DIM> container_t;
+		typedef std::array<T, DIM> container_t;
 	};
 	/** Dynamic size version */
 	template <typename T>
@@ -1251,7 +1179,7 @@ namespace nanoflann
 			float epsError = 1 + searchParams.eps;
 
 			distance_vector_t dists; // fixed or variable-sized container (depending on DIM)
-			dists.assign((DIM > 0 ? DIM : BaseClassRef::dim), 0); // Fill it with zeros.
+			std::fill(dists.begin(), dists.end(), 0); // Fill it with zeros.
 			DistanceType distsq = this->computeInitialDistances(*this, vec, dists);
 			searchLevel(result, vec, BaseClassRef::root_node, distsq, dists, epsError);  // "count_leaf" parameter removed since was neither used nor returned to the user.
             return result.full();
@@ -1320,7 +1248,7 @@ namespace nanoflann
 
 		void computeBoundingBox(BoundingBox& bbox)
 		{
-			bbox.resize((DIM > 0 ? DIM : BaseClassRef::dim));
+			resize(bbox, (DIM > 0 ? DIM : BaseClassRef::dim));
 			if (dataset.kdtree_get_bbox(bbox))
 			{
 				// Done! It was implemented in derived class
@@ -1571,8 +1499,8 @@ namespace nanoflann
                 return false;
 			float epsError = 1 + searchParams.eps;
 
-			distance_vector_t dists; // fixed or variable-sized container (depending on DIM)
-			dists.assign((DIM > 0 ? DIM : BaseClassRef::dim) , 0); // Fill it with zeros.
+			distance_vector_t dists; // fixed or variable-sized container (depending on DIM)			
+			std::fill(dists.begin(), dists.end(), 0); // Fill it with zeros.
 			DistanceType distsq = this->computeInitialDistances(*this, vec, dists);
 			searchLevel(result, vec, BaseClassRef::root_node, distsq, dists, epsError);  // "count_leaf" parameter removed since was neither used nor returned to the user.
             return result.full();
@@ -1634,7 +1562,8 @@ namespace nanoflann
 
 		void computeBoundingBox(BoundingBox& bbox)
 		{
-			bbox.resize((DIM > 0 ? DIM : BaseClassRef::dim));
+			resize(bbox, (DIM > 0 ? DIM : BaseClassRef::dim));
+
 			if (dataset.kdtree_get_bbox(bbox))
 			{
 				// Done! It was implemented in derived class
