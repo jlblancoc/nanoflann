3b3770bdd12b81fd5a21371694e66c90dbae276f
Julien Schueller <schueller@phimeca.com>
2024-08-24 00:06:29 +0200

    Bump version
    Version in header does not match tagged version

--- include/nanoflann.hpp
+++ include/nanoflann.hpp
@@ -61,7 +61,7 @@
 #include <vector>
 
 /** Library version: 0xMmP (M=Major,m=minor,P=patch) */
-#define NANOFLANN_VERSION 0x155
+#define NANOFLANN_VERSION 0x160
 
 // Avoid conflicting declaration of min/max macros in Windows headers
 #if !defined(NOMINMAX) && \
