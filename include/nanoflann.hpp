92911c0bc382e4b287330219bc720ca2b30b2857
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-12-22 17:37:14 +0100

    Update version in nanoflann.hpp to 1.9.0

--- include/nanoflann.hpp
+++ include/nanoflann.hpp
@@ -68,7 +68,7 @@
 #include <vector>
 
 /** Library version: 0xMmP (M=Major,m=minor,P=patch) */
-#define NANOFLANN_VERSION 0x180
+#define NANOFLANN_VERSION 0x190
 
 // Avoid conflicting declaration of min/max macros in Windows headers
 #if !defined(NOMINMAX) && (defined(_WIN32) || defined(_WIN32_) || defined(WIN32) || defined(_WIN64))
