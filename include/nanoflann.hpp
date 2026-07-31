c7305f591ddcd103073f7c87ec578abd8e86b764
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2026-07-31 10:03:43 +0200

    bump release version in .h

--- include/nanoflann.hpp
+++ include/nanoflann.hpp
@@ -86,9 +86,9 @@
 #include <vector>
 
 /** Library version as a decimal string "MAJOR.MINOR.PATCH" */
-#define NANOFLANN_VERSION_STRING "1.10.1"
+#define NANOFLANN_VERSION_STRING "1.11.0"
 /** Library version: 0xMMmmPP (MM=Major, mm=minor, PP=patch) */
-#define NANOFLANN_VERSION 0x010A01
+#define NANOFLANN_VERSION 0x010B00
 
 // Avoid conflicting declaration of min/max macros in Windows headers
 #if !defined(NOMINMAX) && (defined(_WIN32) || defined(_WIN32_) || defined(WIN32) || defined(_WIN64))
